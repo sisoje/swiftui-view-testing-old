e5682e1b99f3d6c0bd47c4c7a94244daf4cd7d71
Lazar Otasevic <lazar.otasevic@emirates.com>
2024-07-12 17:11:32 +0400

    try fix mise

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,8 @@
 cd HostApp
 git clean -fdx
-mise x tuist generate --no-open
+mise install tuist@4.20.0
+mise use tuist@4.20.0
+tuist generate --no-open
 xcodebuild \
   -project HostApp.xcodeproj \
   -scheme HostApp \
