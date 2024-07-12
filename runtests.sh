409992561f47f60a70dfff12584a276f96df844e
Lazar Otasevic <lazar.otasevic@emirates.com>
2024-07-12 15:56:34 +0400

    mise file

--- runtests.sh
+++ runtests.sh
@@ -1,10 +1,9 @@
 cd HostApp
 git clean -fdx
-tuist generate --no-open
+mise x tuist generate --no-open
 xcodebuild \
   -project HostApp.xcodeproj \
   -scheme HostApp \
   -sdk iphonesimulator \
   -destination 'platform=iOS Simulator,name=iPhone 15' \
   test
-  
